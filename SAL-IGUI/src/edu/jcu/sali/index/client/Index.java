2f140640c704b6559d881e1dafe34965428f8808
blackstrype@gmail.com <blackstrype@gmail.com@8a9da2b0-9155-0410-a43d-fb39449fa3ae>
2008-09-18 00:48:53 +0000

    Added minor functionality to demonstrating the populating of the device list.  More work needs to be done for sure.

--- SAL-IGUI/src/edu/jcu/sali/index/client/Index.java
+++ SAL-IGUI/src/edu/jcu/sali/index/client/Index.java
@@ -1,9 +1,16 @@
 package edu.jcu.sali.index.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class Index implements EntryPoint {
+	public ListBox sensorLB;
+	private static final int SENSOR_LB_VISIBLE_ITEMS = 10;
 
 	public void onModuleLoad() {
 		DockPanel dock = new DockPanel();
@@ -14,6 +21,40 @@ public class Index implements EntryPoint {
 		dock.add(new HTML("South"),DockPanel.SOUTH);
 		
 		RootPanel.get("sal-igui").add(dock);
+		
+		RootPanel.get("titleDiv").add(new HTML("SAL-I - Sensor Networks Interface"));
+		
+		InitializeSensorList();
+		PopulateSensorList();
+	}
+	
+	// Initialize the Widgets and such for displaying and configuring the list of sensors
+	private void InitializeSensorList()
+	{
+		sensorLB = new ListBox();
+		sensorLB.setVisibleItemCount(SENSOR_LB_VISIBLE_ITEMS);
+		
+		ClickListener listener = new ClickListener()
+        {
+			public void onClick(Widget sender) {
+				int selectedIndex = ((ListBox) sender).getSelectedIndex();
+				RootPanel.get("sensorDetailsDiv").clear();
+				RootPanel.get("sensorDetailsDiv").add(new HTML("details for sensor #" + selectedIndex));
+			}
+        };
+		sensorLB.addClickListener(listener);
+		
+		RootPanel.get("sensorListDiv").add(new HTML("List of connected sensors."));
+		RootPanel.get("sensorListDiv").add(sensorLB);
+	}
+	
+	// Populate the list of sensors with the retrieved data from SAL
+	public void PopulateSensorList()
+	{
+		// This is temporary code
+		for(int i = 0; i < 5; i++)
+			sensorLB.addItem("sensor" + i);
+		// TODO: Implement AJAX to retrieve list of sensors from SAL
 	}
 
 }
