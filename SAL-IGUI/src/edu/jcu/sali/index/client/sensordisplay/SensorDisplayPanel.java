31f817eca3e4b30877da12d66824020d4e36ad5e
blackstrype@gmail.com <blackstrype@gmail.com@8a9da2b0-9155-0410-a43d-fb39449fa3ae>
2008-10-06 06:36:40 +0000

    Changes to SensorDisplayPanel.  Added a demonstration of video capability within the sensor list display panel.

--- SAL-IGUI/src/edu/jcu/sali/index/client/sensordisplay/SensorDisplayPanel.java
+++ SAL-IGUI/src/edu/jcu/sali/index/client/sensordisplay/SensorDisplayPanel.java
@@ -6,7 +6,23 @@ import com.google.gwt.user.client.ui.HTML;
 public class SensorDisplayPanel extends DockPanel {
 	
 	public SensorDisplayPanel() {
-		this.add(new HTML("<h1>Sensor Display</h1>"), DockPanel.CENTER);
+		this.add(new HTML("<h1>Sensor Display</h1>"), DockPanel.NORTH);
+		InsertVideo();
+	}
+	
+	private void InsertVideo()
+	{
+		String videoText = "";
+		videoText += "<object CLASSID='clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B' width='700' height='700' CODEBASE='http://www.apple.com/qtactivex/qtplugin.cab'>\n"
+				   + "<param name='src' value='media/sample_iTunes.mov'>\n"
+				   + "<param name='qtsrc' value='media/sample_iTunes.mov'>\n"
+				   + "<param name='autoplay' value='false'>\n"
+				   + "<param name='loop' value='false'>\n"
+				   + "<param name='controller' value='true'>\n"
+				   + "<embed src='media/sample_iTunes.mov' qtsrc='media/sample_iTunes.mov'></embed>\n"
+				   + "</object>\n";
+		
+		add(new HTML(videoText), DockPanel.CENTER);
 	}
 
 }
