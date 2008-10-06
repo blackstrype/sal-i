73c596f7b8231013619326730b4b45f946cfd3a8
blackstrype@gmail.com <blackstrype@gmail.com@8a9da2b0-9155-0410-a43d-fb39449fa3ae>
2008-10-06 07:15:49 +0000

    Minor changes to SensorDisplayPanel functionality.  Removed unnessecary html code from index.html.

--- SAL-IGUI/src/edu/jcu/sali/index/client/sensordisplay/SensorDisplayPanel.java
+++ SAL-IGUI/src/edu/jcu/sali/index/client/sensordisplay/SensorDisplayPanel.java
@@ -8,9 +8,13 @@ public class SensorDisplayPanel extends DockPanel {
 	public SensorDisplayPanel() {
 		this.add(new HTML("<h1>Sensor Display</h1>"), DockPanel.NORTH);
 		InsertVideo();
+		//InsertImage();
 	}
 	
-	private void InsertVideo()
+	// Insert an HTML widget for displaying a video file.
+	// TODO: Streaming video
+	// TODO: Access SAL server for streaming video
+	public void InsertVideo()
 	{
 		String videoText = "";
 		videoText += "<object CLASSID='clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B' width='700' height='700' CODEBASE='http://www.apple.com/qtactivex/qtplugin.cab'>\n"
@@ -23,6 +27,15 @@ public class SensorDisplayPanel extends DockPanel {
 				   + "</object>\n";
 		
 		add(new HTML(videoText), DockPanel.CENTER);
+	}
+	
+	// Insert an HTML widget for displaying a JPEG image
+	public void InsertImage()
+	{
+		String imageText = "";
+		imageText += "<img src='images/sample.jpg'>\n";
+		
+		add(new HTML(imageText), DockPanel.SOUTH);
 	}
 
 }
