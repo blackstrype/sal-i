ae9fb45f4f000c0c5dcd484fd1f036dbbf7b77d2
marc.hammerton@web.de <marc.hammerton@web.de@8a9da2b0-9155-0410-a43d-fb39449fa3ae>
2008-10-21 04:55:31 +0000

    started implementing periodical update of sensor list (not finished)

--- SAL-IGUI/src/edu/jcu/sali/index/server/sensorlist/SensorListServiceImpl.java
+++ SAL-IGUI/src/edu/jcu/sali/index/server/sensorlist/SensorListServiceImpl.java
@@ -32,13 +32,24 @@ public class SensorListServiceImpl extends RemoteServiceServlet implements
 	public ArrayList<ArrayList<String>> getSensorList() throws Exception {
 		ArrayList<ArrayList<String>> sensorList = new ArrayList<ArrayList<String>>();
 
+		try {
 		SMLDescriptions listDesc = client.getSensorListActive();
-		for (SMLDescription smlDesc : listDesc.getDescriptions()) {
+		if(listDesc != null) {
+			for (SMLDescription smlDesc : listDesc.getDescriptions()) {
+				ArrayList<String> sensor = new ArrayList<String>();
+				sensor.add(Integer.toString(smlDesc.getSID()));
+				sensor.add(smlDesc.getProtocolType());
+				sensor.add(smlDesc.getSensorAddress());
+				sensor.add(smlDesc.getProtocolName());
+				sensorList.add(sensor);
+			}
+		}
+		} catch (Exception ex) {
 			ArrayList<String> sensor = new ArrayList<String>();
-			sensor.add(Integer.toString(smlDesc.getSID()));
-			sensor.add(smlDesc.getProtocolType());
-			sensor.add(smlDesc.getSensorAddress());
-			sensor.add(smlDesc.getProtocolName());
+			sensor.add("0");
+			sensor.add(null);
+			sensor.add(null);
+			sensor.add(null);
 			sensorList.add(sensor);
 		}
 
